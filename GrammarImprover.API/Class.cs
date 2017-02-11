03fe2b1532eeeebbd5320d8611725c9b31d74aaa
Andrii <apr718@yandex.ru>
2017-02-11 22:39:03 +0300

    Classes1 changed to Classes

--- GrammarImprover.API/Class.cs
+++ GrammarImprover.API/Class.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GrammarImprover.BLL
+namespace GrammarImprover.API
 {
-    public class Class1
+    public class Class
     {
     }
 }
